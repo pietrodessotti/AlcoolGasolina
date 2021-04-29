3bfb99fd905663cf304ae1972561f4bfbd1a9aec
Pietro Dessotti <pietrohdessotti@gmail.com>
2021-04-29 15:56:31 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,23 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String _resultado = "Informe os valores";
+  TextEditingController alcoolController = TextEditingController();
+  TextEditingController gasolinaController = TextEditingController();
+
+  void _calculaCombustivelIdeal() {
+    setState(() {
+      double vAlcool = double.parse(alcoolController.text.replaceAll(',', '.'));
+      double vGasolina =
+          double.parse(gasolinaController.text.replaceAll(',', '.'));
+      double proporcao = vAlcool / vGasolina;
+
+      _resultado =
+          (proporcao < 0.7) ? "Abasteça com Álcool" : "Abasteça com Gasolina";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,53 +41,59 @@ class _HomeState extends State<Home> {
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Icon(
-              Icons.local_gas_station,
-              size: 150.0,
-              color: Colors.lightBlue[900],
-            ),
-            TextFormField(
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Valor do Álcool",
-                  labelStyle: TextStyle(color: Colors.lightBlue[900])),
-              style: TextStyle(color: Colors.lightBlue[900], fontSize: 26.0),
-            ),
-            TextFormField(
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Valor do Gasolina",
-                  labelStyle: TextStyle(color: Colors.lightBlue[900])),
-              style: TextStyle(color: Colors.lightBlue[900], fontSize: 26.0),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-              child: Container(
-                height: 50.0,
-                child: ElevatedButton(
-                  child: Text('Verificar'),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.lightBlue[900],
-                    onPrimary: Colors.white,
-                    textStyle: TextStyle(
-                      fontSize: 25.0,
+        child: Form(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Icon(
+                Icons.local_gas_station,
+                size: 150.0,
+                color: Colors.lightBlue[900],
+              ),
+              TextFormField(
+                controller: alcoolController,
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Valor do Álcool",
+                    labelStyle: TextStyle(color: Colors.lightBlue[900])),
+                style: TextStyle(color: Colors.lightBlue[900], fontSize: 26.0),
+              ),
+              TextFormField(
+                controller: gasolinaController,
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Valor do Gasolina",
+                    labelStyle: TextStyle(color: Colors.lightBlue[900])),
+                style: TextStyle(color: Colors.lightBlue[900], fontSize: 26.0),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                child: Container(
+                  height: 50.0,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      _calculaCombustivelIdeal();
+                    },
+                    child: Text('Verificar'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.lightBlue[900],
+                      onPrimary: Colors.white,
+                      textStyle: TextStyle(
+                        fontSize: 25.0,
+                      ),
                     ),
                   ),
-                  onPressed: () {},
                 ),
               ),
-            ),
-            Text(
-              "Resultado",
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.lightBlue[900], fontSize: 26.0),
-            )
-          ],
+              Text(
+                _resultado,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.lightBlue[900], fontSize: 26.0),
+              )
+            ],
+          ),
         ),
       ),
     );
